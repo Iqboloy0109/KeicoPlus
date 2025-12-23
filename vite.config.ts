1ec327a33884ce924dbf47cad0d460f8f68b9d37
Jeymsbond97 <aminjonovtoxirbek@gmail.com>
2025-12-23 15:09:12 +0900

    feat: created .env files

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: Number(process.env.PORT) || 3003,
-    host: true,
-  },
-  preview: {
-    port: Number(process.env.PORT) || 3003,
-    host: true,
-  },
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const port = Number(env.PORT) || 3003;
+
+  return {
+    plugins: [react()],
+    server: {
+      port,
+      host: true,
+    },
+    preview: {
+      port,
+      host: true,
+    },
+  };
+});
